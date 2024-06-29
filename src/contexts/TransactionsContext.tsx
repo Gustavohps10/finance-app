89b9a09862ad7c0087a07dce9afff0bb1bffb5cd
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-06-29 08:35:43 -0300

    feat: change fetchAPI to axios

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useEffect, useState } from 'react'
+import { api } from '../lib/axios'
 
 interface Transaction {
   description: string
@@ -30,18 +31,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   }, [])
 
   async function fetchTransactions(query?: string) {
-    const url = new URL('http://localhost:3333/transactions')
-
-    if (query) {
-      url.searchParams.append('q', query)
-    }
-
-    const response = await fetch(url.href)
-    const data = await response.json()
-
-    console.log(url.href)
-
-    setTransactions(data)
+    const response = await api.get('/transactions', {
+      params: {
+        q: query,
+      },
+    })
+    setTransactions(response.data)
   }
   return (
     <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
