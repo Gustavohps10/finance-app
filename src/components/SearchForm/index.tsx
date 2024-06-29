94f77d29216527ad522d70814610396d72b00a91
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-06-29 04:54:41 -0300

    feat: add zod schema to search form

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,12 +1,42 @@
 import { MagnifyingGlass } from 'phosphor-react'
 import { Button } from '../Button'
 import * as S from './styled'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const SearchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof SearchFormSchema>
 
 export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(SearchFormSchema),
+  })
+
+  function handleSearchTransactions(data: SearchFormInputs) {
+    console.log(data)
+  }
+
   return (
-    <S.SearchFormContainer>
-      <input type="text" placeholder="Procurar transação" />
-      <Button type="submit" $outline="outlined" $color="primary">
+    <S.SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Procurar transação"
+        {...register('query')}
+      />
+      <Button
+        type="submit"
+        $outline="outlined"
+        $color="primary"
+        disabled={isSubmitting}
+      >
         <MagnifyingGlass size={20} />
         Buscar
       </Button>
