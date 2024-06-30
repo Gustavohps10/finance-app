26c1a47a491ad5a52adb36ce351f04df1044d481
Gustavo Henrque <gustavoh.santos735@gmail.com>
2024-06-30 03:54:37 -0300

    chore: change request baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:
-    'https://my-json-server.typicode.com/Gustavohps10/finance-app/transactions',
+  baseURL: 'https://my-json-server.typicode.com/Gustavohps10/finance-app/',
 })
